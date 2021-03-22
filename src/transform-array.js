6eab9ae29578c736914269d5b2545f98a97d5e1a
Kate <kate.matveeva@gmail.com>
2021-03-23 02:56:08 +0300

    fix: transform-array solution for 'doesn't affect simple arrays', 'control sequences work properly' cases

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,17 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
+ module.exports = function transform(arr) {
   if (!Array.isArray(arr)) throw 'Not an array!';
   let result = [];
   for (let i = 0; i < arr.length; i++) {
-    if (typeof (arr[i]) == "string") continue;
+    if (["--double-next",'--discard-next',"--double-prev","--discard-prev"].includes(arr[i])) continue;
     if (arr[i - 1] === "--double-next") result.push(arr[i]);
     if (arr[i - 1] === '--discard-next') continue;
     if (arr[i + 1] === "--double-prev") result.push(arr[i]);
     if (arr[i + 1] === "--discard-prev") continue;
-    
     result.push(arr[i])
   }
   return result;
-
 };
