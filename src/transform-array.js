0babfa9de5e222330261db1eb42d7695f3acb2c8
Kate <kate.matveeva@gmail.com>
2021-03-23 01:30:57 +0300

    feat: basic implementation transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw 'Not an array!';
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof (arr[i]) == "string") continue;
+    if (arr[i - 1] === "--double-next") result.push(arr[i]);
+    if (arr[i - 1] === '--discard-next') continue;
+    if (arr[i + 1] === "--double-prev") result.push(arr[i]);
+    if (arr[i + 1] === "--discard-prev") continue;
+    
+    result.push(arr[i])
+  }
+  return result;
+
 };
