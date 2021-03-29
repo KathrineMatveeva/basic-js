1b47b6db047a9c89b3985dd25a4e2c033e07abe3
Kate <kate.matveeva@gmail.com>
2021-03-29 22:34:15 +0300

    feat: add error handling

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (arguments.length !== 2) { throw new Error };
+    if (str) { throw new Error };
+    if (key) { throw new Error };
+
+  }
+
+  decrypt(str, key) {
+    if (arguments.length !== 2) { throw new Error };
+    if (str) { throw new Error };
+    if (key) { throw new Error };
+
   }
 }
 
